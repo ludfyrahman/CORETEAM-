fb142ddbef5afcc21ed3bffb77e68f6ca1200b38
nafislestamanta <53818473+nafislestamanta@users.noreply.github.com>
2023-05-24 18:51:30 +0700

    typo pemanggil count inspeksi boat

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -8,9 +8,9 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">Inspeksi Hari ini</p>
                                 <h5 class="font-weight-bolder">
-                                    <?= number_format($data[0])?>
+                                    <?= number_format($data[0]) ?>
                                 </h5>
-                                
+
                             </div>
                         </div>
                         <div class="col-4 text-end">
@@ -30,9 +30,9 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">Inspeksi Mobil</p>
                                 <h5 class="font-weight-bolder">
-								<?= number_format($data[1])?>
+                                    <?= number_format($data[1]) ?>
                                 </h5>
-                                
+
                             </div>
                         </div>
                         <div class="col-4 text-end">
@@ -52,9 +52,9 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">Inspeksi Truk</p>
                                 <h5 class="font-weight-bolder">
-								<?= number_format($data[2])?>
+                                    <?= number_format($data[2]) ?>
                                 </h5>
-                                
+
                             </div>
                         </div>
                         <div class="col-4 text-end">
@@ -74,9 +74,9 @@
                             <div class="numbers">
                                 <p class="text-sm mb-0 text-uppercase font-weight-bold">Inspeksi Boat</p>
                                 <h5 class="font-weight-bolder">
-								<?= number_format($data[2])?>
+                                    <?= number_format($data[3]) ?>
                                 </h5>
-                                
+
                             </div>
                         </div>
                         <div class="col-4 text-end">
@@ -90,23 +90,23 @@
         </div>
     </div>
     <div class="row mt-4">
-		<div class="col-md-12 mb-4" style="height:500px">
+        <div class="col-md-12 mb-4" style="height:500px">
             <div class="card card-carousel overflow-hidden h-100 p-0">
                 <div id="carouselExampleCaptions" class="carousel slide h-100" data-bs-ride="carousel">
                     <div class="carousel-inner border-radius-lg h-100">
-                        <?php 
-						for ($i=0; $i < 10; $i++) { 
-						?>
-						<div class="carousel-item h-100 <?= $i == 0 ? 'active' : '' ?>" style="background-image: url('<?= base_url('assets/img/sliders/slide'.($i+1).'.jpeg') ?>'); background-size: cover;">
-                            <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
-                                <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
-                                    <i class="ni ni-camera-compact text-dark opacity-10"></i>
+                        <?php
+                        for ($i = 0; $i < 10; $i++) {
+                        ?>
+                            <div class="carousel-item h-100 <?= $i == 0 ? 'active' : '' ?>" style="background-image: url('<?= base_url('assets/img/sliders/slide' . ($i + 1) . '.jpeg') ?>'); background-size: cover;">
+                                <div class="carousel-caption d-none d-md-block bottom-0 text-start start-0 ms-5">
+                                    <div class="icon icon-shape icon-sm bg-white text-center border-radius-md mb-3">
+                                        <i class="ni ni-camera-compact text-dark opacity-10"></i>
+                                    </div>
+                                    <h5 class="title position-relative text-white mb-1" style="z-index:2"><?= APP_TITLE ?></h5>
+                                    <p class='desc position-relative text-white' style="z-index:2"><?= APP_DESC ?></p>
                                 </div>
-                                <h5 class="title position-relative text-white mb-1" style="z-index:2"><?= APP_TITLE ?></h5>
-                                <p class='desc position-relative text-white' style="z-index:2"><?= APP_DESC ?></p>
                             </div>
-                        </div>
-						<div class='overlay'></div>
+                            <div class='overlay'></div>
                         <?php } ?>
                     </div>
                     <button class="carousel-control-prev w-5 me-3" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -124,7 +124,7 @@
             <div class="card z-index-2 ">
                 <div class="card-header pb-0 pt-3 bg-transparent">
                     <h6 class="text-capitalize">Inspeksi</h6>
-                    
+
                 </div>
                 <div class="card-body p-3">
                     <div class="chart">
@@ -133,5 +133,5 @@
                 </div>
             </div>
         </div>
-        
+
     </div>
