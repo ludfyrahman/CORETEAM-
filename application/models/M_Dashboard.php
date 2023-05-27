2abc3225eaa8c8cab529df0f26e11eaeb054f400
Mochamad Ludfi Rahman <ludfyrahman@192.168.1.5>
2023-05-27 10:49:54 +0700

    fix daashboard feedback

--- application/models/M_Dashboard.php
+++ application/models/M_Dashboard.php
@@ -10,29 +10,39 @@ class M_Dashboard extends CI_Model
     }
 
 	public function getDataDashboard(){
-		// kalkulasi
-		$allInspection = $this->db->get('inspeksi')->num_rows();
+		// kalkulasi filter by date now
+		$allInspection = $this->db->query('SELECT * FROM inspeksi where date(tgl_inspeksi)  = date(NOW())')->num_rows();
 		$carInspection = $this->db->get_where('inspeksi', ['id_category' => 2])->num_rows();
 		$truckInspection = $this->db->get_where('inspeksi', ['id_category' => 1])->num_rows();
 		$boatInspection = $this->db->get_where('inspeksi', ['id_category' => 3])->num_rows();
-
-		// $charts = $this->db->query("SELECT DATE_FORMAT(tgl_inspeksi, '%M') as bulan, COUNT(*) as jumlah FROM inspeksi GROUP BY bulan")->result_array();
-		$labels = $this->db->query("SELECT DATE_FORMAT(tgl_inspeksi, '%Y - %m - %d') as hari FROM inspeksi GROUP BY hari")->result_array();
-		$labelData = [];
-		foreach ($labels as $key => $label) {
-			$labelData[] = $label['hari'];
-		}
+		
+		// query for get all data in chart
 		$charts = [];
+		// query for get all category
 		$categories = $this->db->get('category')->result_array();
-		foreach ($categories as $key => $category) {
-			$list = $this->db->query("SELECT DATE_FORMAT(tgl_inspeksi, '%Y - %m - %d') as hari, COUNT(*) as jumlah FROM inspeksi where id_category='$category[id_category]' GROUP BY hari")->result_array();
-			$data = [];
-			foreach ($list as $key => $li) {
-				$data[] = $li['jumlah'];
-			}
-			$attribute = ['label' => $category['category'], 'color' => 'green', 'fill' => false, 'data' => $data];
+		// query for get all data in chart
+		$lists = $this->db->query("SELECT u.nama,COUNT(*) as jumlah,u.id_user FROM inspeksi i JOIN user u on i.inspected_by=u.id_user where  date(tgl_inspeksi)  = date(NOW()) GROUP BY inspected_by")->result_array();
+		// define color for chart
+		$category_color = ['rgba(167,225,185,0.5)', 'rgba(54, 162, 235, 0.5)', 'rgba(192, 75, 140, 0.5)'];
+		$labels = [];
+		// loop for get all label
+		foreach ($lists as $key => $cat) {
+			$labels[] = $cat['nama'];
+		}
+		// loop for get all data in chart
+		foreach ($categories as $key => $cat) {
+				$data = [];
+				// loop for get all data in chart
+				foreach ($lists as $childKey => $l) {
+					// query for get calculate data per use
+					$li = $this->db->query("SELECT u.nama,COUNT(*) as jumlah FROM inspeksi i JOIN user u on i.inspected_by=u.id_user where inspected_by='$l[id_user]' and id_category='$cat[id_category]' and  date(tgl_inspeksi)  = date(NOW()) GROUP BY inspected_by")->row_array();
+					$data[] = $li['jumlah'] ?? 0;
+				}
+			// define attribute for chart
+			$attribute = ['label' => $cat['category'], 'color' => $category_color[$key], 'fill' => false, 'data' => $data];
+			// set value for array value charts
 			$charts[] = $attribute;
 		}
-		return [$allInspection, $carInspection, $truckInspection, $boatInspection, $charts, $labelData];
+		return [$allInspection, $carInspection, $truckInspection, $boatInspection, $charts,$labels, $category_color];
 	}
 }
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -126,20 +126,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-6 mb-lg-0 mb-4">
-            <div class="card z-index-2 ">
-                <div class="card-header pb-0 pt-3 bg-transparent">
-                    <h6 class="text-capitalize">Inspeksi</h6>
-
-                </div>
-                <div class="card-body p-3">
-                    <div class="chart">
-                        <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 mb-lg-0 mb-4">
+        <div class="col-lg-12 mb-lg-0 mb-4">
             <div class="card z-index-2 ">
                 <div class="card-header pb-0 pt-3 bg-transparent">
                     <h6 class="text-capitalize">Inspeksi</h6>
