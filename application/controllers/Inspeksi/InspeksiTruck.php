fc9dba3157aa1adeb6c3c8527db5d4e358307a3f
Mochamad Ludfi Rahman <ludfyrahman@192.168.1.7>
2023-05-29 15:32:12 +0700

    update data compress

--- application/controllers/Inspeksi/InspeksiTruck.php
+++ application/controllers/Inspeksi/InspeksiTruck.php
@@ -250,7 +250,6 @@ class InspeksiTruck extends CI_Controller
 
         if ($file != '') {
             $config['file_name']       = $file;
-            $config['max_size']        = '2048';
             $config['allowed_types']   = 'jpg|jpeg|png';
             $config['source_image']    = $_FILES['file']['tmp_name'];
             $config['upload_path']     = './uploads/';
@@ -260,7 +259,20 @@ class InspeksiTruck extends CI_Controller
 
             if ($this->upload->do_upload('file')) {
                 $uploadImage = $this->upload->data();
-
+				$filePath = $uploadImage['full_path'];
+				$imageTemp = $config['source_image']; 
+				$imageSize = convert_filesize($uploadImage['file_size']); 
+				
+				// Compress size and upload image 
+				$compressedImage = compressImage($imageTemp, $uploadImage['full_path'], 75); 
+				
+				if($compressedImage){ 
+					$compressedImageSize = filesize($compressedImage); 
+					$compressedImageSize = convert_filesize($compressedImageSize); 
+					
+				}else{ 
+					echo json_encode(array('status' => 2, 'type' => 'error', 'msg' => 'Gagal Compress file', 'desc' => 'Gagal Kompress Gambar'));
+				} 
                 $this->db->trans_begin();
 
                 // menghapus file lama
